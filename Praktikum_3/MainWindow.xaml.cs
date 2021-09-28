6cf8bc29ecb2036b7227c89b377047fe51c4ed93
TheAlexGo <alex280702@mail.ru>
2021-09-28 10:41:48 +0300

    Выполнение седьмого задания

--- Praktikum_3/MainWindow.xaml.cs
+++ Praktikum_3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,20 @@ namespace Praktikum_3
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Обработчик наведения на объект Run.
+        private void run_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Делаем Popup видимым.
+            popLink.IsOpen = true;
+        }
+        // Переход по ссылке.
+        private void lnk_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = ((Hyperlink)sender).NavigateUri.ToString();
+            // Запускаем приложение, которое ассоциируется с файлом.
+            Process.Start(fileName);
         }
     }
 }
