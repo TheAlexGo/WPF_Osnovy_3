1055468ba611bbd6a79789c9ba1d95128301045e
TheAlexGo <alex280702@mail.ru>
2021-09-28 13:47:07 +0300

    Выполнение 4 варианта

--- Praktikum_3/MainWindow.xaml.cs
+++ Praktikum_3/MainWindow.xaml.cs
@@ -29,36 +29,55 @@ namespace Praktikum_3
             InitializeComponent();
         }
 
-        private dynamic getPage()
+        private dynamic getDataJson(string location)
         {
             WebClient client = new WebClient() { Encoding = System.Text.Encoding.UTF8 };
-            string urlMain= "http://worldtimeapi.org/api/timezone/Europe/London";
-            string japan = urlMain;
-            string text = client.DownloadString(japan);
+            long unixTime = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+            string urlMain = String.Format("https://maps.googleapis.com/maps/api/timezone/json?location={0}&timestamp={1}&key={2}", 
+                location,
+                unixTime,
+                "AIzaSyDZXNyu8SBDGP8sTHAduhTYFidcsPjwHqs"
+                );
+            string text = client.DownloadString(urlMain);
             return JsonConvert.DeserializeObject(text);
         }
-
-        private string getTime()
+        private DateTime getTimeNow()
         {
-            dynamic obj = getPage();
-            DateTime date = new DateTime(1970, 1, 1).AddSeconds(int.Parse(obj["unixtime"]));
-
-            return date.ToString();
+            return DateTime.Now;
+        }
+        private string getTime(string location)
+        {
+            dynamic obj = getDataJson(location);
+            int dstOffset = obj["dstOffset"];
+            int rawOffset = obj["rawOffset"];
+            DateTime date = getTimeNow().AddSeconds(-10800);
+            DateTime currentDate = date.AddSeconds(dstOffset + rawOffset);
+            return currentDate.ToString();
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton pressed = (RadioButton)sender;
             string type = pressed.Content.ToString();
-            TextBlock result = (TextBlock)TextBlockResult;
+            TextBlock result = TextBlockResult;
             switch (type)
             {
-                case "Системная дата":
-                    MessageBox.Show(getTime().ToString()); ;
+                case "Системное время":
+                    result.Text = getTimeNow().ToString();
+                    break;
+                case "Лондон":
+                    result.Text = getTime("51.528308,-0.3817765");
+                    break;
+                case "Токио":
+                    result.Text = getTime("35.5062647,138.6458125");
+                    break;
+                case "Чикаго":
+                    result.Text = getTime("41.875732, -87.623766");
                     break;
                 default:
                     break;
             }
+            TextBlockResult.Visibility = Visibility.Visible;
         }
     }
 }
