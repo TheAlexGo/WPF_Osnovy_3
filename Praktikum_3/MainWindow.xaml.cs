b51a6fb825ce1d7ba3308f90a6f5704f01d229ed
TheAlexGo <alex280702@mail.ru>
2021-09-28 18:11:02 +0300

    Добавление словаря для упрощения кода

--- Praktikum_3/MainWindow.xaml.cs
+++ Praktikum_3/MainWindow.xaml.cs
@@ -24,6 +24,14 @@ namespace Praktikum_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<string, string> options = new Dictionary<string, string>
+        {
+            { "SystemTime", "now" },
+            { "TokyoTime", "35.5062647,138.6458125" },
+            { "ChicagoTime", "41.875732, -87.623766" },
+            { "LondonTime", "51.528308,-0.3817765" }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,16 +49,13 @@ namespace Praktikum_3
             string text = client.DownloadString(urlMain);
             return JsonConvert.DeserializeObject(text);
         }
-        private DateTime getTimeNow()
-        {
-            return DateTime.Now;
-        }
+
         private string getTime(string location)
         {
             dynamic obj = getDataJson(location);
             int dstOffset = obj["dstOffset"];
             int rawOffset = obj["rawOffset"];
-            DateTime date = getTimeNow().AddSeconds(-10800);
+            DateTime date = DateTime.Now.AddSeconds(-10800);
             DateTime currentDate = date.AddSeconds(dstOffset + rawOffset);
             return currentDate.ToString();
         }
@@ -58,25 +63,12 @@ namespace Praktikum_3
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton pressed = (RadioButton)sender;
-            string type = pressed.Content.ToString();
             TextBlock result = TextBlockResult;
-            switch (type)
-            {
-                case "Системное время":
-                    result.Text = getTimeNow().ToString();
-                    break;
-                case "Лондон":
-                    result.Text = getTime("51.528308,-0.3817765");
-                    break;
-                case "Токио":
-                    result.Text = getTime("35.5062647,138.6458125");
-                    break;
-                case "Чикаго":
-                    result.Text = getTime("41.875732, -87.623766");
-                    break;
-                default:
-                    break;
-            }
+
+            result.Text = options[pressed.Name] == "now" 
+                ? DateTime.Now.ToString() 
+                : getTime(options[pressed.Name]);
+
             TextBlockResult.Visibility = Visibility.Visible;
         }
     }
