fcfc7879ce5ba64fa1db3fa283fe4ef671ea5eba
TheAlexGo <alex280702@mail.ru>
2021-09-28 10:26:46 +0300

    Выполнение третьего задания

--- Praktikum_3/MainWindow.xaml.cs
+++ Praktikum_3/MainWindow.xaml.cs
@@ -25,15 +25,21 @@ namespace Praktikum_3
             InitializeComponent();
         }
 
-        private void txt_SelectionChanged(object sender, RoutedEventArgs e)
+        private void lst_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            if (txtSelection == null)
-                return;
-            txtSelection.Text = String.Format("Выбрано с позиции {0}; количество символов == {1}; текст == \"{2}\"",
-                 txt.SelectionStart,
-                 txt.SelectionLength,
-                 txt.SelectedText
-             );
+            if (e.OriginalSource is CheckBox)
+                lst.SelectedItem = e.OriginalSource;
+            txtSelection.Text = String.Format("Позиция {0}.\r\nСостояние {1}.",
+            lst.SelectedIndex,
+            ((CheckBox)lst.SelectedItem).IsChecked);
+        }
+        private void cmd_CheckAllItems(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (CheckBox item in lst.Items)
+                if (item.IsChecked == true)
+                    sb.AppendLine(item.Content + " отмечен. ");
+            txtSelection.Text = sb.ToString();
         }
     }
 }
