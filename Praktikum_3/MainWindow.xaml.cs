6eb077ec7e7770e528632727250e04571a7cb284
TheAlexGo <alex280702@mail.ru>
2021-09-28 12:23:02 +0300

    Инициализация проекта

--- Praktikum_3/MainWindow.xaml.cs
+++ Praktikum_3/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +27,38 @@ namespace Praktikum_3
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private dynamic getPage()
+        {
+            WebClient client = new WebClient() { Encoding = System.Text.Encoding.UTF8 };
+            string urlMain= "http://worldtimeapi.org/api/timezone/Europe/London";
+            string japan = urlMain;
+            string text = client.DownloadString(japan);
+            return JsonConvert.DeserializeObject(text);
+        }
+
+        private string getTime()
+        {
+            dynamic obj = getPage();
+            DateTime date = new DateTime(1970, 1, 1).AddSeconds(int.Parse(obj["unixtime"]));
+
+            return date.ToString();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            string type = pressed.Content.ToString();
+            TextBlock result = (TextBlock)TextBlockResult;
+            switch (type)
+            {
+                case "Системная дата":
+                    MessageBox.Show(getTime().ToString()); ;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
